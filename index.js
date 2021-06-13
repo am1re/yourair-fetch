714c522ff7f55763b7430eccbc25c580e261b5ac
Amir Abu Shamala <36200444+am1re@users.noreply.github.com>
2021-06-14 01:09:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { connect } from "mongoose"
 import intizializeFetching from './fetch'
 import indexRouter from './routes/index'
 
-connect("mongodb://localhost:27017/yourairdb1", { useNewUrlParser: true, useUnifiedTopology: true })
+connect("mongodb+srv://dbUser:Lw0wwRiysffGOams@cluster0.vxgoq.mongodb.net/yourairdb1?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         const app = express()
         app.use('/', indexRouter)
